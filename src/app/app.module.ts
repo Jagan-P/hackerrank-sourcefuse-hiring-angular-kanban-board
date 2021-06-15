a740e3b28d235cdb777dbfea248c29fd19f42966
Jagan.P <jagan.p@plantix.net>
2021-06-15 22:16:53 +0530

    read only file change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { KanbanBoard } from './kanbanBoard/kanbanBoard.component';
 //Angular Router Module
@@ -17,6 +17,7 @@ import {RouterTestingModule} from '@angular/router/testing';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterTestingModule,
     RouterModule.forRoot([
       { path:'', component: KanbanBoard }
